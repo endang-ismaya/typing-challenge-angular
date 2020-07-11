9bd35e42d9ee63c90f8a5de42edc4d0038548d05
Endang.Ismaya <endang.ismaya@gmail.com>
2020-07-11 08:51:41 +0700

    11/7/2020: add styling and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText: string = lorem.sentence();
+  isSuccess: boolean = false;
+
+  onInput(value: string): void {
+    if (value === this.randomText) {
+      this.isSuccess = true;
+    }
+  }
 }
